cb5c6e47e2bc20cf19350e3d20d544cc7c2ee5a7
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-21 10:48:02 +0530

    Improved exception handling using filter and first methods

--- playground/views.py
+++ playground/views.py
@@ -5,9 +5,9 @@ from store.models import Product
 
 
 def say_hello(request):
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
+    #better way to handle exception
+    #first() return NONE if no object does not exist
+    product = Product.objects.filter(pk=0).first()
+    
     return render(request, 'hello.html', {'name': 'Yash'})
 
