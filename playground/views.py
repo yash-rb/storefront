2178fbeaea0604220cbb09b804716fcb28336f0d
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-22 00:55:18 +0530

    Querying tagged items (generic relationships)

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product, OrderItem, Order
+from tags.models import TaggedItem  
 
 
 def say_hello(request):
-    #Get all the products which are ordered and sort acc to title
-    products = Product.objects.filter(id__in=
-                                    OrderItem.objects.values('product_id').distinct()).order_by('title')
-    #get products of last 5 orders" 
-    orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    #Querying generic Relationships using contentType
+    
+    contentType = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects.\
+        select_related('tag')\
+            .filter(
+                content_type = contentType,
+                object_id = 1
+            )
     
     
-    return render(request, 'hello.html', {'name': 'Yash', 'products':list(products), 'orders':list(orders)})
+    
+    return render(request, 'hello.html', {'name': 'Yash', 'tags':list(query_set)})
 
