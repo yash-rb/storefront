c171ab7098d52926acd9ee73e582071cbf5a67d8
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-21 12:16:25 +0530

    Complex Query lookups

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    #better way to handle exception
-    #first() return NONE if no object does not exist
-    product = Product.objects.filter(pk=0).first()
+    #Get all the products which are ordered and sort acc to title
+    products = Product.objects.filter(id__in=
+                                    OrderItem.objects.values('product_id').distinct()).order_by('title')
     
-    return render(request, 'hello.html', {'name': 'Yash'})
+    
+    return render(request, 'hello.html', {'name': 'Yash', 'products':list(products)})
 
