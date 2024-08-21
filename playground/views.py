51a2de306015ab88f7f4a32e067a9cfdbac24cae
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-22 01:04:51 +0530

    Imlementing custom object manager for TaggedItem class

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product, OrderItem, Order
+from store.models import Product
 from tags.models import TaggedItem  
 
 
 def say_hello(request):
     #Querying generic Relationships using contentType
-    
-    contentType = ContentType.objects.get_for_model(Product)
-    query_set = TaggedItem.objects.\
-        select_related('tag')\
-            .filter(
-                content_type = contentType,
-                object_id = 1
-            )
-    
-    
-    
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
+
     return render(request, 'hello.html', {'name': 'Yash', 'tags':list(query_set)})
 
--- tags/models.py
+++ tags/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, object_type, obj_id):
+        
+        contentType = ContentType.objects.get_for_model(object_type)
+        return TaggedItem.objects.\
+            select_related('tag')\
+                .filter(
+                    content_type = contentType,
+                    object_id = obj_id
+                )
+                
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
