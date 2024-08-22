d48803b657cbed2978bb37bb2537f038f459d6ea
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-22 19:31:29 +0530

    Added functionality to check orders of a particular customer

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,8 @@
+from typing import Any
 from django.db.models import Count
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from . import models
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
@@ -23,10 +26,25 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    
+    @admin.display(ordering='orders_count') 
+    def orders_count(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id':str(customer.id)
+            })
+               )
+        orders_represent = f"{customer.orders_count} Orders" if customer.orders_count >0 else 0
+        return format_html('<a href="{}">{}</a>', url, orders_represent)
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(orders_count=Count('order'))
     
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
