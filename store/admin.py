e633d63bc9545b452e13181ee7dceba22d36a437
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-22 09:50:11 +0530

    Added computed columns (inventory status) to Products model

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status', 'inventory']
     list_editable = ['unit_price']
     list_per_page = 10
+    
+    @admin.display(ordering=['inventory'])
+    def inventory_status(self, product):
+        if product.inventory < 20:
+            return 'low'
+        return 'OK'
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
