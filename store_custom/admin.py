17ea0f1f01b1ec49d49484af155598b1427429d7
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-23 01:01:43 +0530

    Removing dependencies of apps using store_custom app

---
+++ store_custom/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from store.admin import ProductAdmin
+from tags.models import TaggedItem
+from store.models import Product
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+# Register your models here.
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+    extra = 0
+    
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
--- storefront/settings.py
+++ storefront/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
+    'store_custom',
     'tags',
-    'likes'
+    'likes',
 ]
 
 MIDDLEWARE = [
