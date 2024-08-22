2f4ceaa635231ccf7ee1cd3dd13cf0ea1a28725a
Yash Rajbhar <yashraj78289@gmail.com>
2024-08-22 10:17:31 +0530

    Registered order model and added related fields

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'inventory']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
     
     @admin.display(ordering=['inventory'])
     def inventory_status(self, product):
@@ -22,4 +26,12 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     
 admin.site.register(models.Collection)
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at','customer', ]
+    list_per_page = 10
+    list_select_related = ['customer']
+    
+   
 
